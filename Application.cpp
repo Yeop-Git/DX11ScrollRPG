cb3072a2fd38b8d2ef213f214d91b4ee42704be2
Yeop-Git <quto1010@gmail.com>
2026-08-27 20:55:56 +0900

    DX11ScrollRPG Window 1-3
    - InitializeDirectX() 구현
    - Render() 구현 : 남청색 화면 렌더
    - ComPtr : DirectX COM 객체 특화 스마트 포인터
    
    InitializeDirectX()
    -> SwapChain 설정
    -> Device&Context&SwapChain 생성
    -> BackBuffer 획득
    -> RenderTargetView 생성
    -> RenderTarget 연결

--- Application.cpp
+++ Application.cpp
@@ -1,5 +1,8 @@
 #include "Application.h"
 
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "dxgi.lib")
+
 namespace
 {
 	LRESULT CALLBACK WindowProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
@@ -14,6 +17,36 @@ namespace
 		return DefWindowProc(hwnd, message, wParam, lParam);
 	}
 }
+
+
+bool Application::Initialize(HINSTANCE hInstance, int nCmdShow)
+{
+	if (!CreateMainWindow(hInstance, nCmdShow))
+	{
+		return false;
+	}
+		
+	if (!InitializeDirectX())
+	{
+		return false;
+	}
+
+	return true;
+}
+
+int Application::Run()
+{
+	MSG message{};
+
+	while (ProcessMessages())
+	{
+		Update();
+		Render();
+	}
+
+	return static_cast<int>(message.wParam);
+}
+
 
 bool Application::CreateMainWindow(HINSTANCE hInstance, int nCmdShow)
 {
@@ -59,22 +92,53 @@ bool Application::CreateMainWindow(HINSTANCE hInstance, int nCmdShow)
 	return true;
 }
 
-bool Application::Initialize(HINSTANCE hInstance, int nCmdShow)
+bool Application::InitializeDirectX()
 {
-	return CreateMainWindow(hInstance, nCmdShow);
-}
+	DXGI_SWAP_CHAIN_DESC desc{};
 
-int Application::Run()
-{
-	MSG message{};
+	desc.BufferDesc.Width = kWInodwWidth;
+	desc.BufferDesc.Height = kWindowHeight;
+	desc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
 
-	while (ProcessMessages())
-	{
-		//Update();
-		//Render();
-	}
+	desc.SampleDesc.Count = 1;
 
-	return static_cast<int>(message.wParam);
+	desc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	desc.BufferCount = 2;
+
+	desc.OutputWindow = hwnd_;
+	desc.Windowed = TRUE;
+	desc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
+
+	HRESULT hr = D3D11CreateDeviceAndSwapChain(
+		nullptr,
+		D3D_DRIVER_TYPE_HARDWARE,
+		nullptr,
+		0,
+		nullptr,
+		0,
+		D3D11_SDK_VERSION,
+		&desc,
+		swapChain_.GetAddressOf(),
+		device_.GetAddressOf(),
+		nullptr,
+		context_.GetAddressOf()
+	);
+
+	if (FAILED(hr)) return false;
+
+	Microsoft::WRL::ComPtr<ID3D11Texture2D> backBuffer;
+
+	hr = swapChain_->GetBuffer(0, IID_PPV_ARGS(backBuffer.GetAddressOf()));
+
+	if (FAILED(hr)) return false;
+
+	hr = device_->CreateRenderTargetView(backBuffer.Get(), nullptr, renderTargetView_.GetAddressOf());
+
+	if (FAILED(hr)) return false;
+
+	context_->OMSetRenderTargets(1, renderTargetView_.GetAddressOf(), nullptr);
+
+	return true;
 }
 
 bool Application::ProcessMessages()
@@ -90,4 +154,16 @@ bool Application::ProcessMessages()
 		DispatchMessage(&message);
 	}
 	return true;
+}
+
+void Application::Update()
+{
+	// Update logic here 
+}
+
+void Application::Render()
+{
+	const float clearColor[4] = { 0.1f, 0.15f, 0.25f, 1.0f };
+	context_->ClearRenderTargetView(renderTargetView_.Get(), clearColor);
+	swapChain_->Present(1, 0);
 }
--- Application.h
+++ Application.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <Windows.h>
+#include <d3d11.h>
+#include <wrl/client.h>
 
 class Application
 {
@@ -13,11 +15,20 @@ public:
 
 private:
 	bool CreateMainWindow(HINSTANCE hInstance, int nCmdShow);
+	bool InitializeDirectX();
+
 	bool ProcessMessages();
+	void Update();
+	void Render();
 
 private:
 	HWND hwnd_ = nullptr;
 
 	static constexpr int kWInodwWidth = 1280;
 	static constexpr int kWindowHeight = 720;
+
+	Microsoft::WRL::ComPtr<ID3D11Device> device_;
+	Microsoft::WRL::ComPtr<ID3D11DeviceContext> context_;
+	Microsoft::WRL::ComPtr<IDXGISwapChain> swapChain_;
+	Microsoft::WRL::ComPtr<ID3D11RenderTargetView> renderTargetView_;
 };
