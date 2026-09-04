4b0d07535902e953210d77776549bea7dea0eb70
Yeop-Git <quto1010@gmail.com>
2026-09-04 18:56:58 +0900

    6-1 GameObject 추가
    - Transform, Collider, Active만 관리하는 GameObject Class 추가
    - Entity는 GameObject를 상속받아 Update(float deltaTime)만을 요구하는 추상계층으로 변경
    - GameObject에 OnEnable, OnDisable추가, SetActive에서 호출
    - 이후 Monster의 기초적인 Object Pooling에서 OnEnable, OnDisable 활용 예정.

--- Game/Entity/Entity.h
+++ Game/Entity/Entity.h
@@ -1,31 +1,14 @@
 #pragma once
 
-#include "Transform.h"
-#include "Collider.h"
-#include "../../Engine/Graphics/RenderInfo.h"
+#include "GameObject.h"
 
 // 월드에서 Update되는 모든 객체를 Entity로 추상화
-class Entity
+class Entity : GameObject
 {
 public:
-	Transform transform;
-	Collider collider;
-
 	// 소멸자 virtual
 	virtual ~Entity() = default;
 
 	// Update를 순수 virtual 함수로 하여 자식에서 구현을 강제
 	virtual void Update(float deltaTime) = 0;
-	virtual RenderInfo GetRenderInfo() const = 0;
-
-	AABB GetBodyBox() const
-	{
-		return collider.GetBounds(transform);
-	}
-
-	bool IsActive() const { return active_; }
-
-	void SetActive(bool active) { active_ = active; }
-
-	bool active_ = true;
 };
---
+++ Game/Entity/GameObject.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "Transform.h"
+#include "Collider.h"
+#include "../../Engine/Graphics/RenderInfo.h"
+
+// 모든 게임오브젝트를 추상화, Transform과 Collider 소유
+class GameObject
+{
+public:
+	Transform transform;
+	Collider collider;
+
+	// 소멸자 virtual
+	virtual ~GameObject() = default;
+
+	// Update를 순수 virtual 함수로 하여 자식에서 구현을 강제
+	virtual RenderInfo GetRenderInfo() const = 0;
+
+	// AABB Box Collider
+	AABB GetBodyBox() const
+	{
+		return collider.GetBounds(transform);
+	}
+
+	// Active 관리
+	bool IsActive() const { return active_; }
+
+	void SetActive(bool active) 
+	{ 
+		if (active_ == active) return;
+		active_ = active; 
+
+		if (active_) OnEnable();
+		else OnDisable();
+	}
+
+	// OnEnable, OnDisable 추가
+protected:
+	virtual void OnEnable(){}
+	virtual void OnDisable() {}
+
+private:
+	bool active_ = true;
+};
